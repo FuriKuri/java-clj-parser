2a41bd1563444eec4b54368dff08b092d17fd5f0
Theo Pack <tf.pack@googlemail.com>
2012-03-14 22:43:35 +0100

    add function for magic number

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject java-clj-parser "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
