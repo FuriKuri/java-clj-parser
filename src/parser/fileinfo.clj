b3a442ec710d7f140f292034aa74604f8c4ce3e4
Theo Pack <tf.pack@googlemail.com>
2012-03-15 20:26:40 +0100

    add functions to get minor and major verison

--- src/parser/fileinfo.clj
+++ src/parser/fileinfo.clj
@@ -1,5 +1,18 @@
 (ns parser.fileinfo
   (:use [parser.file]))
 
-(defn magic-number [input-stream]
-  {:magic-number (read-next-bytes-as-hex-str input-stream 4)})
+(defmacro register-parser [name bytes-to-read]
+  (let [sym-name (symbol name)
+        key-name (keyword name)
+        is-sym (symbol "input-stream")]
+    `(defn ~sym-name
+       ~(str "Read next " bytes-to-read " bytes to get " name)
+       [~is-sym]
+       {~key-name (read-next-bytes-as-hex-str ~is-sym ~bytes-to-read)})))
+
+(register-parser magic-number 4)
+(register-parser minor-version 2)
+(register-parser major-version 2)
+
+;;(defn magic-number [input-stream]
+;;  {:magic-number (read-next-bytes-as-hex-str input-stream 4)})
--- test/parser/test/fileinfo.clj
+++ test/parser/test/fileinfo.clj
@@ -14,3 +14,25 @@
                                   "aabbccdd"]]]
     (let [input-stream (mock-stream stream-bytes)]
       (is (= {:magic-number result} (magic-number input-stream))))))
+
+(deftest read-minor-version
+  (doseq [[stream-bytes result] [
+                                 ['("00" "00")
+                                  "0000"]
+                                 ['("00" "00" "01" "02")
+                                  "0000"]
+                                 ['("20" "01" "DD" "EE")
+                                  "2001"]]]
+    (let [input-stream (mock-stream stream-bytes)]
+      (is (= {:minor-version result} (minor-version input-stream))))))
+
+(deftest read-major-version
+  (doseq [[stream-bytes result] [
+                                 ['("00" "00")
+                                  "0000"]
+                                 ['("00" "00" "01" "02")
+                                  "0000"]
+                                 ['("20" "01" "DD" "EE")
+                                  "2001"]]]
+    (let [input-stream (mock-stream stream-bytes)]
+      (is (= {:major-version result} (major-version input-stream))))))
