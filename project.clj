673b6356e444fc08b06264fcf0d9a23654bfb0e1
Theo Pack <tf.pack@googlemail.com>
2012-03-19 22:48:28 +0100

    update version and description

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject java-clj-parser "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject java-clj-parser "0.0.1"
+  :description "Java Bytecode parser written in Clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :main parser)
